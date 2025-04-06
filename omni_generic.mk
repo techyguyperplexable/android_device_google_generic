8898444462755b4a6d22491f22ad01dd4768ec44
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-04-06 04:28:12 +0200

    generic: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: google Model: mainline Script version: 3.0.0

---
+++ omni_generic.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from generic device
+$(call inherit-product, device/google/generic/device.mk)
+
+PRODUCT_DEVICE := generic
+PRODUCT_NAME := omni_generic
+PRODUCT_BRAND := google
+PRODUCT_MODEL := mainline
+PRODUCT_MANUFACTURER := google
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="oriole-user 15 BP1A.250305.019 13003188 release-keys"
+
+BUILD_FINGERPRINT := google/oriole/oriole:15/BP1A.250305.019/13003188:user/release-keys
