27ae882970447136ccdb2a5f14f2998d783a83a1
techyguyperplexable <154611145+techyguyperplexable@users.noreply.github.com>
2025-04-17 17:14:23 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_generic-user
-add_lunch_combo omni_generic-userdebug
-add_lunch_combo omni_generic-eng
+add_lunch_combo twrp_generic-user
+add_lunch_combo twrp_generic-userdebug
+add_lunch_combo twrp_generic-eng
