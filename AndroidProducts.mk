b0b7ddddc15fc203e03f0baf670566f79ea48c7b
techyguyperplexable <154611145+techyguyperplexable@users.noreply.github.com>
2025-04-17 17:25:09 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_generic.mk
+    $(LOCAL_DIR)/twrp_generic.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_generic-user \
-    omni_generic-userdebug \
-    omni_generic-eng
+    twrp_generic-user \
+    twrp_generic-userdebug \
+    twrp_generic-eng
